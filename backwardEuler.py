af72fd2abc79c5dfb5d8ac0ecf879d6a3512a05b
Ashish Gupta <haitomns4173@gmail.com>
2025-07-17 11:10:40 +0545

    Backward Euler Method

---
+++ backwardEuler.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+
+def backward_euler(f, x0, y0, xn, n):
+    x = x0
+    y = y0
+    h = (xn - x0) / n
+
+    x_values = [x0]
+    y_values = [y0]
+
+    for i in range(n):
+        y1 = y + h * (f(x, y))
+        y = y + h * (f(x + h, y1))
+        x = x + h
+        x_values.append(x)
+        y_values.append(y)
+
+    return x_values, y_values
+
+def plot_results(x_values, y_values):
+    plt.plot(x_values, y_values, marker='o', linewidth=2)
+    plt.title('Backward Euler Method Results')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.grid()
+    plt.show()
+
+
+x_results, y_results = backward_euler(lambda x, y: (y**2 - x**2) / (y**2 + x**2), 0, 1, 1, 20)
+
+plot_results(x_results, y_results)
+for x, y in zip(x_results, y_results):
+    print(f"{x:.4f} \t {y:.4f}")
